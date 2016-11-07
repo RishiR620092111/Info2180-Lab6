b581282f38a633917b2d88545105fe81dad3ae02
RishiR620092111 <rishi.ramsamooj96@gmail.com>
2016-11-06 17:13:49 -0800

    Update request.php

--- request.php
+++ request.php
@@ -16,3 +16,47 @@ $definition = [
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
+<?php
+header("Content-type: text/xml");
+print("");
+$string = <<<XML
+<ol>
+	<li>
+		<definition>
+			Definition = a statement of the exact meaning of a word, especially in a dictionary.
+		</definition>
+		<author>
+		    <strong> Author: Rishi </strong>
+		</author>
+		<example>
+		    <em> Example: Definitions are the definitions of words</em>
+		</example>
+	</li>
+	<li>
+		<definition>
+			Bar = a place that sells alcholic beverages.
+		</definition>
+		<author>
+		    <strong> Author: Cat </strong>
+		</author>
+		<example>
+		    <em> Example: I drink at bars</em>
+		</example>
+	</li>
+	<li>
+		<definition>
+			Ajax = technique which involves the use of javascript and xml.
+		</definition>
+		<author>
+		    <strong> Author: Dog </strong>
+		</author>
+		<example>
+		    <em> Example: I code in ajax</em>
+		</example>
+	</li>
+  </ol>
+XML;
+$xml = new SimpleXMLElement($string);
+echo $xml->asXML();
+?>
