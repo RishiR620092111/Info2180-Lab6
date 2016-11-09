6c501be11faf2b0162be09c4de164c3bf9e5017b
RishiR620092111 <rishi.ramsamooj96@gmail.com>
2016-11-09 14:51:59 -0800

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function loadDefinitions()
+{
+    var query = document.getElementById("queryTxtBox").value.toLowerCase();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            document.getElementById("result").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "request.php?q="+ query, true);
+    xhttp.send();
+}
